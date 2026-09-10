76ef440aa9401d2d8f6fa05d8a49c2e56468a194
morganssch <coltm576@gmail.com>
2026-09-10 08:32:42 -0500

    Add project files.

---
+++ Stacks_Joseph_Morgan/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_Joseph_Morgan
+{
+     class Card
+    {
+
+
+        public string Number { get; }
+
+        public string Suit { get; }
+
+        public Card(string numParam, string suitParam)
+        {
+            Number = numParam;
+            Suit = suitParam;
+        }
+    }
+}
---
+++ Stacks_Joseph_Morgan/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Stacks_Joseph_Morgan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card[] myCardsArray = new Card[]
+            {
+
+                new Card("Ace", "Spades"),
+                new Card("Jack", "Clubs"),
+                new Card("Queen", "Diamonds"),
+                new Card("Queen", "Hearts"),
+                new Card("10", "Hearts"),
+                new Card("2", "Diamonds"),
+                new Card("6", "Spades"),
+                new Card("9", "Clubs"),
+                new Card("Ace", "Diamonds")
+            };
+
+            Stack<Card> startingDeck = new Stack<Card>(myCardsArray);
+
+            Console.WriteLine("Cards in Deck:");
+            foreach(Card card in startingDeck)
+            {
+                Console.WriteLine($"{card.Number} of {card.Suit}!");
+            }
+
+            Console.WriteLine($"Cards in the deck: {startingDeck.Count}");
+
+            List<Card> myHand = new List<Card>();
+            myHand.Add(new Card("Jack", "Diamonds"));
+            myHand.Add(new Card("8", "Hearts"));
+            myHand.Add(new Card("3", "Diamonds"));
+
+            Console.WriteLine("Got any 8's?");
+            if(startingDeck.Count > 0)
+            {
+                myHand.Add(startingDeck.Pop());
+            }
+
+            Console.WriteLine($"Cards in the stack: {startingDeck.Count}");
+
+            startingDeck.Push(myHand[0] );
+            myHand.RemoveAt(0);
+
+            Console.WriteLine("Cards in Deck:");
+            foreach (Card card in startingDeck)
+            {
+                Console.WriteLine($"{card.Number} of {card.Suit}!");
+            }
+        }
+    }
+}
